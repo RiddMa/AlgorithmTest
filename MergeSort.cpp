ff8b9b9dcfe87d97e2e0494469b90842462919ae
Ridd <1017601960@qq.com>
2020-09-28 22:09:10 +0800

    nice

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,11 +1,60 @@
-int MergeSort(int A[], int left, int right)
+#include <iostream>
+
+void Merge(int A[], int B[], int left, int mid, int right)
 {
-    if (left < right)
+    int i(1);
+    int j(mid + 1);
+    int k(1);
+    while ((i <= mid) && (j <= right))
     {
-        int mid = (left + right) / 2;
-        MergeSort(A, left, mid);
-        MergeSort(A, mid + 1, right);
-        return 0;
+        if (A[i] <= A[j])
+            B[k++] = A[i++];
+        else
+            B[k++] = A[j++];
     }
-    return -1;
+    if (i > mid)
+    {
+        for (int q = j; q <= right; q++)
+            B[k++] = A[q];
+    }
+    else
+        for (int q = i; q <= mid; q++)
+            B[k++] = A[q];
+}
+
+void MergePass(int A[], int B[], int step, int n)
+{
+    int i(0);
+    for (; i <= n - 2 * step; i = i + 2 * step)
+    {
+        Merge(A, B, i, i + step - 1, i + 2 * step - 1);
+    }
+    if (i + step < n)
+        Merge(A, B, i, i + step - 1, n - 1);
+    else
+        for (int j = i; j <= n - 1; j++)
+            B[j] = A[j];
+}
+
+void MergeSort(int A[], int n)
+{
+    int *B = new int[n];
+    for (int step = 1; step < n;)
+    {
+        MergePass(A, B, step, n);
+        step += step;
+        MergePass(B, A, step, n);
+        step += step;
+    }
+}
+
+int main()
+{
+    int A[] = {7, 4, 2, 9, 1, 0};
+    MergeSort(A, 6);
+    for (int i = 0; i < 6; i++)
+    {
+        std::cout << A[i] << std::endl;
+    }
+    return 0;
 }
